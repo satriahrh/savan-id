7a86bdc54aac80ec174c4e0b50b5b92c3436e6c7
Satria H R Harsono <satriahrh@gmail.com>
2020-11-01 22:54:19 +0700

    baby wear coming soon

---
+++ pages/baby-wear.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import {Button, Divider, Typography} from '@material-ui/core'
+import {makeStyles} from "@material-ui/core/styles";
+import SavanLogoIcon from "../components/icons/savan-logo-icon";
+
+export default function Index() {
+  const classes = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      minHeight: '100vh'
+    },
+    logo: {
+      width: '8em',
+      height: '100%',
+      marginBottom: theme.spacing(4)
+    }
+  }))();
+  return (
+    <div className={classes.root}>
+      <SavanLogoIcon className={classes.logo}/>
+      <Typography variant='h3' as='h1'>
+        Savan Baby Wear
+      </Typography>
+      <Typography variant='h5' as='p' gutterBottom>
+        ... akan segera hadir.
+      </Typography>
+      <Divider />
+      <Typography>Jelajahi bisnis kami yang lain di</Typography>
+      <Button variant='outlined'>beranda</Button>
+    </div>
+  )
+}
