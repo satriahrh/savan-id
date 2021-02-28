5c68c557804f52916beff6d0b0a799fcc2942d2a
Satria H R Harsono <satriahrh@gmail.com>
2021-02-28 23:29:43 +0700

    firestore handle error

--- firebase/firestore.init.js
+++ firebase/firestore.init.js
@@ -1,21 +1,16 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
 
-export function firebaseInit() {
-  try {
-    let firebaseConfig = {
-      apiKey: "AIzaSyCmxXgbawLs-ltxkZmuyv6Nh0_uuYcJtu4",
-      authDomain: "savan-id.firebaseapp.com",
-      databaseURL: "https://savan-id.firebaseio.com",
-      projectId: "savan-id",
-      storageBucket: "savan-id.appspot.com",
-      messagingSenderId: "469976157461",
-      appId: "1:469976157461:web:1d2dbd2c1bc32160ca6d1c",
-      measurementId: "G-NDNPF8BBR2"
-    };
-    firebase.initializeApp(firebaseConfig);
-  } catch(err) {
-    console.log("Firestore initialization error: ", err);
+import 'firebase/firestore';
+import fire from './firebase.init';
+
+const firestore = fire.firestore();
+try {
+  firestore.useEmulator('localhost', 8080);
+} catch (err) {
+  if (err.name === ' FirebaseError') {
+    err.code !== 'failed-precondition' ? console.error(err) : null;
+  } else {
+    console.error(err);
   }
-  return firebase;
 }
+
+export default firestore;
--- pages/products/[...productPath].js
+++ pages/products/[...productPath].js
@@ -29,43 +29,50 @@ import NavigationAppBar from "../../components/navigation/app-bar";
 import {default as RelatedProductsCarousel} from "../../components/products/carousel";
 import {makeStyles} from "@material-ui/core/styles";
 import Footer from "../../components/navigation/footer";
-import {firebaseInit} from "../../firebase/firestore.init";
+import firestore from "../../firebase/firestore.init";
+import Error from 'next/error'
 
-export default function Index() {
-  const router = useRouter();
-  Index.getInitialProps = ({query}) => {
-    return {query}
-  };
-  const productId = getIdFromProductPath(router.query.productPath);
-
-  const [product, setProduct] = useState({loading: true});
-  let productDetail = <h1>Tunggu Ya</h1>
-  if (product.loading) {
-    if (productId) {
-      getProduct(productId).then((product) => {
-        console.log(productId);
-        setProduct({
-          loading: false,
-          detail: product
-        })
-      });
-    }
-  } else {
-    productDetail = <ProductDetail productDetail={product.detail}/>
+export default function Index({data, error}) {
+  if (error || !data) {
+    return <Error statusCode={error?.statusCode} title={error?.title} />
   }
 
   return (
     <>
       <Head>
-        <title>{product.loading ? '' : product.detail.name + ' | Produk Savan'}</title>
+        <title>{data.product.name + ' | Produk Savan'}</title>
         <link rel="icon" href="/icon.svg"/>
       </Head>
       <NavigationAppBar/>
-      {productDetail}
+      <ProductDetail productDetail={data.product} />
       <RelatedProducts/>
       <Footer/>
     </>
   )
+}
+
+export async function getServerSideProps({params}) {
+  try {
+    const productId = getIdFromProductPath(params.productPath);
+    console.log(productId);
+    const product = await getProduct(productId);
+    return {
+      props: {
+        data: {
+          product: product
+        }
+      }
+    };
+  } catch (err) {
+    return {
+      props: {
+        error: {
+          statusCode: 404,
+          title: err.toString(),
+        }
+      }
+    };
+  }
 }
 
 function ProductDetail({productDetail}) {
@@ -325,74 +332,18 @@ function getIdFromProductPath(productPath) {
 }
 
 async function getProduct(id) {
-  const firebase = await firebaseInit();
-  let db = firebase.firestore()
-  db.useEmulator("localhost", 8080);
-  const doc = await db.collection('products').doc(id).get();
-  if (doc.exists) {
-    return doc.data();
-  }
-  return {
-    name: "Jumper Solid",
-    variants: {
-      main: ['Putih', 'Polos', 'Strip', 'Strap'],
-      secondary: ['NB', '3-6m', '6-9m', '9-12m', '1y', '2y', '3y'],
-    },
-    brandName: 'Savan Baby Store',
-    brandColor: '#FFD770',
-    price: 21300,
-    photos: {
-      thumbnails: [`https://via.placeholder.com/200x200/8f8e94/FFFFFF?text=Jumper Nahkoda Abu`],
-      main: [
-        'https://via.placeholder.com/600x600/FFFFFF/8f8e94?text=FotoUtamaPolos',
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoKedua',
-      ],
-      'vPutih': [
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoUtamaPutih',
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoKeduaPutih',
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoKetigaPutih',
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoKeempatPutih',
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoKelimaPutih',
-      ],
-      'vPolos': [
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoUtamaPolos',
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoKeduaPolos',
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoKetigaPolos',
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoKeempatPolos',
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoKelimaPolos',
-      ],
-      'vStrip': [
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoUtamaStrip',
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoKeduaStrip',
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoKetigaStrip',
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoKeempatStrip',
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoKelimaStrip',
-      ],
-      'vStrap': [
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoUtamaStrap',
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoKeduaStrap',
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoKetigaStrap',
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoKeempatStrap',
-        'https://via.placeholder.com/600x600/8f8e94/FFFFFF?text=FotoKelimaStrap',
-      ],
-    },
-    marketplacesUrl: {
-      shopee: `https://shopee.co.id/Fluffy-OJS-Baju-Panjang-Oblong-Rib-untuk-Anak-Bayi-Usia-6-12-bulan-1-pcs-i.277931002.5440958490`,
-    },
-    descriptions: [
-      {
-        summary: "Detail",
-        details: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful."
-      },
-      {
-        summary: "Ukuran",
-        details: "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish."
-      },
-      {
-        summary: "Bahan dan Perawatan",
-        details: "Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat"
-      },
-    ],
+  try {
+    const doc = await firestore.collection('products').doc(id).get();
+    if (doc.exists) {
+      return doc.data();
+    } else {
+      throw 'produk tidak ditemukan';
+    }
+  } catch (err) {
+    if (err.name == 'FirebaseError') {
+      throw 'tidak bisa mengambil data';
+    }
+    throw err;
   }
 }
 
