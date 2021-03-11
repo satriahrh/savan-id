1a1736eae15257bc67373593858514018f4ff9d0
Satria H R Harsono <satriahrh@gmail.com>
2021-03-11 14:17:55 +0700

    env firebase

--- firebase/firestore.js
+++ firebase/firestore.js
@@ -1,18 +1,10 @@
 const firebase = require('firebase');
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCmxXgbawLs-ltxkZmuyv6Nh0_uuYcJtu4",
-  authDomain: "savan-id.firebaseapp.com",
-  databaseURL: "https://savan-id.firebaseio.com",
-  projectId: "savan-id",
-  storageBucket: "savan-id.appspot.com",
-  messagingSenderId: "469976157461",
-  appId: "1:469976157461:web:1d2dbd2c1bc32160ca6d1c",
-  measurementId: "G-NDNPF8BBR2"
-};
-
 if (!firebase.apps.length) {
   try {
+    const firebaseConfigJson = process.env.FIREBASE_CONFIG_JSON;
+    console.log(firebaseConfigJson);
+    const firebaseConfig = JSON.parse(firebaseConfigJson);
+    
     firebase.initializeApp(firebaseConfig);
   } catch (error) {
     console.log('Firebase admin initialization error', error.stack);
@@ -22,7 +14,7 @@ if (!firebase.apps.length) {
 const firestore = firebase.firestore();
 if (process.env.NODE_ENV === 'development') {
   if(typeof window === 'undefined' || !window['_init']) {
-    firestore.useEmulator('localhost', 8080);
+    firestore.useEmulator(process.env.FIRESTORE_EMULATOR_HOST, process.env.FIRESTORE_EMULATOR_PORT);
     if(typeof window !== 'undefined') {
       window['_init'] = true;
     }
