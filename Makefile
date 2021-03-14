e4bd1618dbe692b0adeae7f430cf70b7a6d1114d
Satria H R Harsono <satriahrh@gmail.com>
2021-03-14 22:26:24 +0700

    no emulator for firestore

--- Makefile
+++
@@ -1,5 +0,0 @@
-firestore-dummy:
-	node firebase/firestore-dummy.js
-firestore-emulate:
-	firebase emulators:start --only firestore
-
--- firebase/firestore.js
+++ firebase/firestore.js
@@ -11,13 +11,13 @@ if (!firebase.apps.length) {
 }
 
 const firestore = firebase.firestore();
-if (process.env.NODE_ENV === 'development') {
-  if (typeof window === 'undefined' || !window['_init']) {
-    firestore.useEmulator(process.env.FIRESTORE_EMULATOR_HOST, process.env.FIRESTORE_EMULATOR_PORT);
-    if (typeof window !== 'undefined') {
-      window['_init'] = true;
-    }
-  }
-}
+// if (process.env.NODE_ENV === 'development') {
+//   if (typeof window === 'undefined' || !window['_init']) {
+//     firestore.useEmulator(process.env.FIRESTORE_EMULATOR_HOST, process.env.FIRESTORE_EMULATOR_PORT);
+//     if (typeof window !== 'undefined') {
+//       window['_init'] = true;
+//     }
+//   }
+// }
 
 export default firestore;
